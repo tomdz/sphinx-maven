ad9b4ac8cb954ddca39999887e288e75705a1e48
Thomas Dudziak <thomas@ning.com>
2012-01-15 20:11:45 -0800

    Added a script to update sphinx

---
+++ src/main/build/update-sphinx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]:-$0}); pwd)
+BASE_DIR=$(cd "$SCRIPT_DIR/../../.."; pwd)
+WORK_DIR="target/sphinx-tmp"
+pushd "$BASE_DIR" > /dev/null
+mkdir -p $WORK_DIR
+cd $WORK_DIR
+curl -LO "http://downloads.sourceforge.net/project/jython/jython/2.5.2/jython_installer-2.5.2.jar"
+curl -O "http://peak.telecommunity.com/dist/ez_setup.py"
+java -jar jython_installer-2.5.2.jar -s -d jython -t standard
+./jython/bin/jython ez_setup.py
+./jython/bin/easy_install docutils pygments jinja2 sphinx
+jar cf sphinx.jar -C jython/Lib/site-packages/Sphinx*/ sphinx
+jar uf sphinx.jar -C jython/Lib/site-packages/Jinja2*/ jinja2
+jar uf sphinx.jar -C jython/Lib/site-packages/docutils*/ docutils
+jar uf sphinx.jar -C jython/Lib/site-packages/docutils*/ roman.py
+jar uf sphinx.jar -C jython/Lib/site-packages/docutils*/ 'roman$py.class'
+jar uf sphinx.jar -C jython/Lib/site-packages/Pygments*/ pygments
+mv sphinx.jar "$BASE_DIR/src/main/resources/"
+popd > /dev/null
