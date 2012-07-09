d1cba02ea3eaaad91ce511167df2bde7bc6a84e8
Thomas Dudziak <tomdzk@gmail.com>
2012-07-08 17:53:06 -0700

    Trying to add pdf support

--- src/main/build/update-sphinx.sh
+++ src/main/build/update-sphinx.sh
@@ -9,12 +9,14 @@ curl -LO "http://downloads.sourceforge.net/project/jython/jython/2.5.2/jython_in
 curl -O "http://peak.telecommunity.com/dist/ez_setup.py"
 java -jar jython_installer-2.5.2.jar -s -d jython -t standard
 ./jython/bin/jython ez_setup.py
-./jython/bin/easy_install docutils pygments jinja2 sphinx
+./jython/bin/easy_install docutils pygments jinja2 sphinx rst2pdf
 jar cf sphinx.jar -C jython/Lib/site-packages/Sphinx*/ sphinx
 jar uf sphinx.jar -C jython/Lib/site-packages/Jinja2*/ jinja2
 jar uf sphinx.jar -C jython/Lib/site-packages/docutils*/ docutils
 jar uf sphinx.jar -C jython/Lib/site-packages/docutils*/ roman.py
 jar uf sphinx.jar -C jython/Lib/site-packages/docutils*/ 'roman$py.class'
 jar uf sphinx.jar -C jython/Lib/site-packages/Pygments*/ pygments
+jar uf sphinx.jar -C jython/Lib/site-packages/simplejson*/ simplejson
+jar uf sphinx.jar -C jython/Lib/site-packages/rst2pdf*/ rst2pdf
 mv sphinx.jar "$BASE_DIR/src/main/resources/"
 popd > /dev/null
