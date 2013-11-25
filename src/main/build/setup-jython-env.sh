dfefa68c52ae1be169be8f4e721231854eaae051
Anders Nordström <nordanster@gmail.com>
2013-11-25 14:43:06 +0100

    Rolled back a few unecessary changes

--- src/main/build/setup-jython-env.sh
+++ src/main/build/setup-jython-env.sh
@@ -6,8 +6,8 @@ pushd "$BASE_DIR" > /dev/null
 rm -rf $WORK_DIR
 mkdir -p $WORK_DIR
 cd $WORK_DIR
-wget "http://downloads.sourceforge.net/project/jython/jython/2.5.2/jython_installer-2.5.2.jar"
-wget "http://peak.telecommunity.com/dist/ez_setup.py"
+curl -LO "http://downloads.sourceforge.net/project/jython/jython/2.5.2/jython_installer-2.5.2.jar"
+curl -O "http://peak.telecommunity.com/dist/ez_setup.py"
 java -jar jython_installer-2.5.2.jar -s -d jython -t standard
 ./jython/bin/jython ez_setup.py
 ./jython/bin/easy_install docutils 
@@ -18,7 +18,7 @@ java -jar jython_installer-2.5.2.jar -s -d jython -t standard
 
 # reportlab's default setup doesn work under Jython, so let's install it manually
 # we'll also patch it along the way
-wget http://pypi.python.org/packages/source/r/reportlab/reportlab-2.5.tar.gz
+curl -O http://pypi.python.org/packages/source/r/reportlab/reportlab-2.5.tar.gz
 tar zxf reportlab-2.5.tar.gz
 patch -d reportlab*/src/reportlab -p6 < "$BASE_DIR/src/main/build/reportlab.patch"
 pushd reportlab* > /dev/null
