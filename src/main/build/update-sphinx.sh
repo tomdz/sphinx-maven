9e3a9da704ed0764425a8bb0baf3e6a865cf9020
Olivier Heurtier <olivier.heurtier@free.fr>
2014-04-17 22:31:09 +0200

    Added javasphinx and httpdomain Upgraded to Sphinx 1.2.2 and docutils 0.11

--- src/main/build/update-sphinx.sh
+++ src/main/build/update-sphinx.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]:-$0}); pwd)
 BASE_DIR=$(cd "$SCRIPT_DIR/../../.."; pwd)
 WORK_DIR="target/sphinx-tmp"
 
-/bin/bash "$SCRIPT_DIR/setup-jython-env.sh"
+#/bin/bash "$SCRIPT_DIR/setup-jython-env.sh"
 
 pushd "$BASE_DIR" > /dev/null
 cd $WORK_DIR
@@ -14,5 +14,9 @@ jar uf sphinx.jar -C jython/Lib/site-packages/Pygments*/ pygments
 jar uf sphinx.jar -C jython/Lib/site-packages/simplejson*/ simplejson
 jar uf sphinx.jar -C jython/Lib/site-packages/rst2pdf*/ rst2pdf
 jar uf sphinx.jar -C jython/Lib/site-packages/ reportlab
+jar uf sphinx.jar -C jython/Lib/site-packages/sphinxcontrib_h*/ sphinxcontrib
+jar uf sphinx.jar -C jython/Lib/site-packages/javasphinx*/ javasphinx
+jar uf sphinx.jar -C jython/Lib/site-packages/javalang*/ javalang
+jar uf sphinx.jar -C jython/Lib/site-packages/beautifulsoup4*/ bs4
 mv sphinx.jar "$BASE_DIR/src/main/resources/"
 popd > /dev/null
